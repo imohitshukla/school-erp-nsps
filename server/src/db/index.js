083b6bf98d46aecb907af8c774150d3460cbb731
imohitshukla <mohitshukla57662@gmail.com>
2026-08-07 18:34:57 +0530

    Fix database SSL connection for Render

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -4,6 +4,7 @@ const logger = require('../utils/logger');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 pool.on('connect', () => {
